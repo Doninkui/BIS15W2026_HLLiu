57e32222ffb2b9d59c7cee499090a99f2d6739eb
Leo <wanghouyushenglhj@gmail.com>
2026-03-03 11:49:40 -0800

    lab 15

---
+++ list.R
@@ -0,0 +1,9 @@
+7-filter summaries , pull, count
+8.groupby
+9.pivot_longer,pivot_wider
+10.横向barplot，10.2两个信息barplot
+11.Build line plots, histograms, and density plots，给图加theme
+12.ai
+13.na，数na，移除na，join data frames
+14.map，location
+15shiny app
